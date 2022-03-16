db240fde46b5f94277fe2bfcd9a7b417786ea329
lucasderveaux <86676401+lucasderveaux@users.noreply.github.com>
2022-03-16 12:09:48 +0100

    ik kan een ttl bestand volledig inlezen

--- src/sources/BlueBikeExample.ts
+++ src/sources/BlueBikeExample.ts
@@ -1,29 +1,29 @@
 import type * as RDF from 'rdf-js';
-import {ISource} from './ISource';
-import {Page} from "./Page";
+import { ISource } from './ISource';
+import { Page } from "./Page";
 import { newEngine } from '@treecg/actor-init-ldes-client';
 import { Observation } from '../lib/Observation';
 
-export class BlueBikeExample implements ISource{
+export class BlueBikeExample implements ISource {
     //private triples: RDF.Quad[];
     //private metadata:RDF.Quad[];   
 
-    constructor(){
+    constructor() {
         //Hier zou nog vanalles kunnen komen in het verband met triples en metadat.
         //Misschien moet ik terug met Pages werken want eigenlijk was dat wel een goed systeem.
         //Ja ik ga dat doen dat is super logisch.
     }
 
-    
 
-    getPage():Page{
+
+    getPage(): Page {
         return null;
     }
 
-   
-    async getData():Promise<Observation[]> {
-        
-        return new Promise<Observation[]>((resolve,reject)=>{
+
+    async getData(): Promise<Observation[]> {
+
+        return new Promise<Observation[]>((resolve, reject) => {
             try {
                 let options = {
                     emitMemberOnce: true,
@@ -34,21 +34,28 @@ export class BlueBikeExample implements ISource{
 
                 let LDESClient = newEngine();
                 let eventStreamSync = LDESClient.createReadStream(
-                    "https://www.pieter.pm/Blue-Bike-to-Linked-GBFS/root.ttl",
+                    "https://www.pieter.pm/Blue-Bike-to-Linked-GBFS/history/20220315T075514.ttl",
                     options
                 );
 
-                let data: Observation[] = [];
+                let data: Observation[];
 
-                eventStreamSync.on('data', (member : any) => {
+                let memberURI: any;
+                let quads: RDF.Quad[];
+
+                eventStreamSync.on('data', (member: any) => {
+                    //console.log(member);
                     //het is hier ook met Quads wnat ik heb die representation zo ingesteld
-                    let memberURI = member.id.value;
-                    console.log(memberURI);
-                    let quads=member.quads;
-                    console.log(quads);
+                    console.log(member.id.value);
+                    //console.log(memberURI);
+                   this.analyseData(member.quads);
+                    //console.log(quads);
                 });
                 eventStreamSync.on('end', () => {
                     console.log("No more data!");
+                    //console.log(memberURI);
+                    //console.log(quads);
+                    
                     resolve(data);
                 });
             } catch (e) {
@@ -56,7 +63,56 @@ export class BlueBikeExample implements ISource{
                 return reject(e);
             }
         });
-        
+
+    }
+
+    public analyseData(member: RDF.Quad[]): void {
+        //eerst kijkt die of dat de featureOfInterest al bestaat
+
+        member.forEach((x) => {
+            switch (x.predicate.value) {
+                case "http://purl.org/dc/terms/created": {
+                    console.log("created on:\t" + x.object.value);
+                    break;
+                }
+                case "http://schema.org/name":{
+                    console.log("naam van het station:\t"+x.object.value);
+                    break;
+                }
+                case "http://www.w3.org/2003/01/geo/wgs84_pos#latitude":{
+                    console.log("de latitude is:\t"+x.object.value);
+                    break;
+                }
+                case "http://www.w3.org/2003/01/geo/wgs84_pos#longitude":{
+                    console.log("de longitude is:\t"+x.object.value);
+                    break;
+                }
+                case "https://w3id.org/gbfs#bikes_available":{
+                    console.log("aantal fietsen available:\t"+x.object.value);
+                    break;
+                }
+                case "https://w3id.org/gbfs#docks_in_use":{
+                    console.log("aantal docks in use?\t"+x.object.value);
+                    break;
+                }
+
+            }
+
+        });
+        console.log("\n");
+
+        //CreateFeatureOfInterest
+        //Longitude
+
+        //latitude
+        //id
+
+        //dan kijkt die of dat de observation al bestaat
+
+        //Observation
+        //timestamp
+        //value
+
     }
 
 }
--- src/sources/Page.ts
+++ src/sources/Page.ts
@@ -1,5 +1,7 @@
 import type * as RDF from "rdf-js";
 import * as RdfString from "rdf-string";
+import * as f from "@dexagod/rdf-retrieval"
+import rdfSerializer from "rdf-serialize";
 
 export class Page{
     private triples: RDF.Quad[];
@@ -31,5 +33,13 @@ export class Page{
         this.metadata.forEach(metadata_quad => {metadata.push(RdfString.quadToStringQuad(metadata_quad))})
         serialized['metadata'] = metadata;
         return serialized;
+    }
+
+    public async getSerializedPage(contentType:string ="text/turtle"):Promise<NodeJS.ReadableStream>{
+        let all: RDF.Quad[]=this.getMetada();
+        all = all.concat(this.getTriples());
+        const tripleStream:RDF.Stream<RDF.Quad>=await f.quadArrayToQuadStream(all);
+
+        return rdfSerializer.serialize(tripleStream,{contentType:contentType});
     }
 }
