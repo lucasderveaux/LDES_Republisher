9ae26d4e54b69ec26c2453f444318ad3dcf6f672
lucasderveaux <86676401+lucasderveaux@users.noreply.github.com>
2022-04-28 18:26:42 +0200

    ldes aanpassing

--- src/QuadTheCreator.ts
+++ src/QuadTheCreator.ts
@@ -143,43 +143,140 @@ export class QuadTheCreator {
                 namedNode(idSimpleValue)
             )
         );
+
+        let size = sortedMap.length();
+        let seconds = (24 * 60 * 60) / size;
+
         //  ifc:timeStep [
+        quads.push(
+            quad(
+                blankNode('A'),
+                namedNode('https://w3id.org/ifc/IFC4_ADD1#timeStep_IfcRegularTimeSeries'),
+                blankNode('seconds')
+            )
+        );
         //   a ifc:IfcTimeMeasure;
+        quads.push(
+            quad(
+                blankNode('seconds'),
+                namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                namedNode(' https://w3id.org/ifc/IFC4_ADD1#IfcTimeMeasure')
+            )
+        );
         //   time:seconds "3".
+        quads.push(
+            quad(
+                blankNode('seconds'),
+                namedNode('https://www.w3.org/2006/time#seconds'),
+                literal(seconds.toString(), "https://www.w3.org/2001/XMLSchema#float")
+            )
+        );
         //   ];
         //  ifc:startTime [
+        quads.push(
+            quad(
+                blankNode('A'),
+                namedNode('https://w3id.org/ifc/IFC4_ADD1#startTime_IfcWorkControl'),
+                blankNode('startTime')
+            )
+        );
         //   a ifc:ifcDateTime;
+        quads.push(
+            quad(
+                blankNode('startTime'),
+                namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                namedNode('https://w3id.org/ifc/IFC4_ADD1#IfcDateTime')
+            )
+        );
         //   ifcSimpleValue "2017-04-15T00:00:00+00:00".
+        quads.push(
+            quad(
+                blankNode('startTime'),
+                namedNode('https://w3id.org/ifc/IFC4_ADD1#IfcSimpleValue'),
+                literal(day, 'http://www.w3.org/2001/XMLSchema#dateTime')
+            )
+        );
         //   ];
         //  ifc:endTime [
+        quads.push(
+            quad(
+                blankNode('A'),
+                namedNode('https://w3id.org/ifc/IFC4_ADD1#startTime_IfcWorkControl'),
+                blankNode('endTime')
+            )
+        );
         //   a ifc:ifcDateTime;
-        //   ifc:ifcSimpleValue "2017-04-16T00:00:00+00:00".
+        quads.push(
+            quad(
+                blankNode('endTime'),
+                namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                namedNode('https://w3id.org/ifc/IFC4_ADD1#IfcDateTime')
+            )
+        );
+        //   ifcSimpleValue "2017-04-16T00:00:00+00:00".
+        quads.push(
+            quad(
+                blankNode('endTime'),
+                namedNode('https://w3id.org/ifc/IFC4_ADD1#IfcSimpleValue'),
+                literal(day, 'http://www.w3.org/2001/XMLSchema#dateTime')
+            )
+        );
         //   ];
         //  ifc:values [
+        quads.push(
+            quad(
+                blankNode('A'),
+                namedNode('https://w3id.org/ifc/IFC4_ADD1#values_IfcStructuralLoadConfiguration'),
+                blankNode('timeSeriesList')
+            )
+        );
         //   a ifc:ifcTimeSeriesList;
+        quads.push(
+            quad(
+                blankNode('timeSeriesList'),
+                namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                namedNode('https://w3id.org/ifc/IFC4_ADD1#IfcTimeSeriesValue_List')
+            )
+        );
         //   <http://purl.org/co/size> "2";
+        quads.push(
+            quad(
+                blankNode('timeSeriesList'),
+                namedNode('http://purl.org/co/size'),
+                literal(size, 'https://www.w3.org/2001/XMLSchema#integer')
+            )
+        );
         //   ifc:IfcValue ("12", "11", "23", "459", ... ).
-        //  ].
-
-
 
         for (let key of sortedMap.keys()) {
             let starturl: string = "https://blue-bike.be/observations/" + key;
+
             quads.push(
                 quad(
-                    namedNode(starturl),
-                    namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-                    namedNode('http://www.w3.org/ns/sosa/Observation')
+                    blankNode('timeSeriesList'),
+                    namedNode('https://w3id.org/list#hasContents'),
+                    blankNode(key.toString())
                 )
             );
             quads.push(
                 quad(
-                    namedNode(starturl),
+                    blankNode(key.toString()),
+                    namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                    namedNode(' https://w3id.org/ifc/IFC4_ADD1#IfcSimpleValue')
+                )
+            );
+
+           
+            quads.push(
+                quad(
+                    namedNode(key.toString()),
                     namedNode(idSimpleValue),
                     literal(sortedMap.get(key).toString())
                 )
             );
-
         }
+        //  ].
+
+
     }
 }
